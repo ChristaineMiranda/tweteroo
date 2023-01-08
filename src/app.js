6ae30cbe5a7a963918a9428c176e41d0c7b13c8c
christaine <christaine.miranda@gmail.com>
2023-01-07 21:39:07 -0300

    versão com get para rota sign-up

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@ import express from 'express'
 import cors from 'cors'
 
 const server = express();
-server.use(cors())
+server.use(cors());
+server.use(express.json());
 const PORT = 5000;
 
-server.listen(PORT, ()=>{
+const usuariosArmazenados = [];
+const tweetsArmazenados = [];
+
+server.listen(PORT, () => {
     console.log("Servidor está no ar!");
 })
+
+server.post("/sign-up", (req, res) => {
+
+    const { username, avatar } = req.body;
+    if (username && avatar) {
+        const novoUsuario = { nome: username, avatar: avatar };
+        usuariosArmazenados.push(novoUsuario);
+        res.send("OK");
+        return;
+    }
+
+    res.status(422).send("Por favor, preencha o campo usuário e avatar corretamente")
+})
+
+
+
+
 
